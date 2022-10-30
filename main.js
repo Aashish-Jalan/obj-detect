78797e9c3a1a6d61f42eb81d3996c1502fe60eb0
Aashish Jalan <77002032+Aashish-Jalan@users.noreply.github.com>
2022-10-30 11:56:18 +0530

    Update main.js

--- main.js
+++ main.js
@@ -31,14 +31,15 @@ function draw()
         objectDetector.detect(video,gotResult);
       for (i  = 0 ; 1< objects.length; i++)
       {
-        document.getElementById("status").innerHTML = "Status : Object Detected"
-        document.getElementById("number").innerHTML = "Number of Objects detected are : " + objects.length;
+       
         fill(r,g,b);
         percent = floor(objects[i].confidence*100);
         text(objects[i].label + " " + percent + "%" , objects[i].x +15 , objects[i].y+15);
         noFill();
         stroke(r,g,b);
         rect(objects[i].x , objects[i].y , objects[i].width , objects[i].height ); 
+           document.getElementById("status").innerHTML = "Status : Object Detected";
+        document.getElementById("number").innerHTML = "Number of Objects detected are : " + objects.length;
       }
     }
 }
